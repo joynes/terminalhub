06a68dd2d122d2811afac64c13c90b5c0e4219f5
Johannes Kahlare <joynez11@gmail.com>
2026-03-29 16:20:43 +0200

    Capture release crashes on next launch

--- app/src/main/java/se/joynes/aiterminalhub/AITerminalHubApplication.kt
+++ app/src/main/java/se/joynes/aiterminalhub/AITerminalHubApplication.kt
@@ -2,6 +2,17 @@ package se.joynes.aiterminalhub
 
 import android.app.Application
 import dagger.hilt.android.HiltAndroidApp
+import se.joynes.aiterminalhub.data.logging.AppLogger
+import se.joynes.aiterminalhub.data.logging.CrashReportStore
+import javax.inject.Inject
 
 @HiltAndroidApp
-class AITerminalHubApplication : Application()
+class AITerminalHubApplication : Application() {
+    @Inject lateinit var appLogger: AppLogger
+
+    override fun onCreate() {
+        super.onCreate()
+        CrashReportStore.install(this)
+        CrashReportStore.flushPendingCrash(this, appLogger)
+    }
+}
